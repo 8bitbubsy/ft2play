b5fa90fdfb90a8c3cf4d764a04f053d36458af4a
Olav Sørensen <olav.sorensen@live.no>
2022-01-25 10:55:27 +0100

    Fix possible compiler warning

--- ft2play/make-linux.sh
+++ ft2play/make-linux.sh
@@ -3,7 +3,7 @@
 rm release/other/ft2play &> /dev/null
 echo Compiling, please wait...
 
-gcc -DNDEBUG -DAUDIODRIVER_SDL ../audiodrivers/sdl/*.c ../*.c src/*.c -g0 -lSDL2 -lm -lpthread -Wshadow -Winit-self -Wall -Wno-maybe-uninitialized -Wno-missing-field-initializers -Wno-unused-result -Wno-strict-aliasing -Wextra -Wunused -Wunreachable-code -Wswitch-default -O3 -o release/other/ft2play
+gcc -DNDEBUG -DAUDIODRIVER_SDL ../audiodrivers/sdl/*.c ../*.c src/*.c -g0 -lSDL2 -lm -lpthread -Wshadow -Winit-self -Wall -Wno-uninitialized -Wno-missing-field-initializers -Wno-unused-result -Wno-strict-aliasing -Wextra -Wunused -Wunreachable-code -Wswitch-default -O3 -o release/other/ft2play
 
 rm ../*.o src/*.o &> /dev/null
 
